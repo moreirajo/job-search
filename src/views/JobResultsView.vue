d88ce59363d323c09e052244bbd987921c86e9ff
joamor <joamor@signicat.com>
2023-08-27 01:43:41 +0100

    first commit

---
+++ src/views/JobResultsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="font-bod text-6xl">Job Results page</h1>
+  <button @click="goToHome">Go to Home Page</button>
+</template>
+
+<script>
+export default {
+  name: "JobResultsView",
+  methods: {
+    goToHome() {
+      this.$router.push({ name: "Home" });
+    },
+  },
+};
+</script>
